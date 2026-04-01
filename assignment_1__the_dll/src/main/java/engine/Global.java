bdff69fe40bdfc5e1007279b93f10c58f5fc52fd
Brenton James <261373365+myBCompSc@users.noreply.github.com>
2026-04-01 17:12:17 +1000

    Add files via upload
    - dose compile.
    - dose boot.
    - dose run SIM.
    SIMULATION ERROR - output not correct.
    
    Signed-off-by: Brenton James <261373365+myBCompSc@users.noreply.github.com>

---
+++ assignment_1__the_dll/src/main/java/engine/Global.java
@@ -0,0 +1,114 @@
+package engine;
+
+import java.nio.ByteBuffer;
+import com.sun.jna.Pointer;
+
+public class Global
+{
+    private static char _stat_REG_numberOfCores;
+    private static char _stat_REG_numberOfPraises;
+// public.
+    public Global() 
+    {
+            System.out.printf("entering Global()%n");
+            stat_CLASS_boot0_DECLAIRE_Global();
+            stat_CLASS_boot1_DEFINE_Global();
+            stat_CLASS_boot3_INITIALISE_Global();
+            stat_REG_boot0_DECLAIRE_Global();
+            System.out.printf("exiting Global()%n");
+    }
+    public void dyn_REG_boot1_DEFINE_Global(Framework obj)
+    {
+            System.out.printf("entered dyn_REG_boot1_DEFINE_Global().%n");
+
+            System.out.printf("exiting dyn_REG_boot1_DEFINE_Global().%n");
+    }
+    public void dyn_REG_boot2_SUBSTANTIATE_Global(Framework obj)
+    {
+            System.out.printf("entered dyn_REG_boot2_SUBSTANTIATE_Global().%n");
+
+            System.out.printf("exiting dyn_REG_boot2_SUBSTANTIATE_Global().%n");
+    }
+    public void dyn_REG_boot3_INITIALISE_Global(Framework obj)
+    {
+            System.out.printf("entered dyn_REG_boot3_INITIALISE_Global().%n");
+
+            System.out.printf("exiting dyn_REG_boot3_INITIALISE_Global().%n");
+    }
+    public void dyn_REG_boot4_INSTANTIATE_Global(Framework obj)
+    {
+            System.out.printf("entered dyn_REG_boot4_INSTANTIATE_Global().%n");
+
+            System.out.printf("exiting dyn_REG_boot4_INSTANTIATE_Global().%n");
+    }
+    public static char dyn_REG_get_numberOfCores()
+    {
+            return stat_REG_get_numberOfCores();
+    }
+    public static char dyn_REG_get_numberOfPraises()
+    {
+            return stat_REG_get_numberOfPraises();
+    }
+    public static float stat_CONVERT_Bytes_To_Float(byte[] bytes) 
+    {
+    if (bytes.length != 4) {
+        throw new IllegalArgumentException("Byte array must have exactly 4 bytes.");
+    }
+    ByteBuffer buffer = ByteBuffer.wrap(bytes);
+    return buffer.getFloat();
+    }
+    public static double stat_CONVERT_Bytes_To_Double(byte[] bytes) 
+    {
+    if (bytes.length != 8) {
+        throw new IllegalArgumentException("Byte array must have a length of 8");
+    }
+    ByteBuffer buffer = ByteBuffer.wrap(bytes);
+    return buffer.getDouble();
+    }
+    public static byte[] stat_CONVERT_Float_To_Bytes(float value) 
+    {
+        ByteBuffer buffer = ByteBuffer.allocate(Float.BYTES);
+        buffer.order(java.nio.ByteOrder.LITTLE_ENDIAN);
+        buffer.putFloat(value);
+        return buffer.array();
+    }
+    public static byte[] stat_CONVERT_Double_To_Bytes(double value) 
+    {
+        ByteBuffer buffer = ByteBuffer.allocate(Double.BYTES);
+        buffer.putDouble(value);
+        return buffer.array();
+    }
+	public void stat_CLASS_boot0_DECLAIRE_Global()
+	{
+		System.out.printf("entered stat_CLASS_boot0_DECLAIRE_Global().%n");
+		
+		System.out.printf("exiting stat_CLASS_boot0_DECLAIRE_Global().%n");
+	}
+	private static void stat_CLASS_boot1_DEFINE_Global()
+	{
+		System.out.printf("entered stat_CLASS_boot1_DEFINE_Global().%n");
+		
+		System.out.printf("exiting stat_CLASS_boot1_DEFINE_Global().%n");
+	}
+	private static void stat_CLASS_boot3_INITIALISE_Global()
+	{
+		System.out.printf("entered stat_CLASS_boot3_INITIALISE_Global().%n");
+		
+		System.out.printf("exiting stat_CLASS_boot3_INITIALISE_Global().%n");
+	}
+	private static void stat_REG_boot0_DECLAIRE_Global()
+	{
+		System.out.printf("entered stat_REG_boot0_DECLAIRE_Global().%n");
+		
+		System.out.printf("exiting stat_REG_boot0_DECLAIRE_Global().%n");
+	}
+// private.
+    public static char stat_REG_get_numberOfCores()
+	{
+		return _stat_REG_numberOfCores;
+	}
+    public static char stat_REG_get_numberOfPraises()
+	{
+		return _stat_REG_numberOfPraises;
+	}
+}
